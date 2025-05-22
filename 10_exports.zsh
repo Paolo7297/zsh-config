1902f59b98c10c967ad15a7e07e5b8a52e348725
Paolo Casellati <3812347+Paolo7297@users.noreply.github.com>
2025-05-22 17:28:36 +0200

    Added zsh-fzf-history-search

--- 10_exports.zsh
+++ 10_exports.zsh
@@ -2,28 +2,3 @@
 if [[ -d "$HOME/.local/share/JetBrains/Toolbox/scripts" ]]; then
   export PATH="$PATH:/home/paolo/.local/share/JetBrains/Toolbox/scripts"
 fi
-
-# History
-
-export HISTFILE=$ZDOTDIR/.zsh_history
-export HISTSIZE=100000
-export SAVEHIST=$HISTSIZE
-
-setopt EXTENDED_HISTORY          # Write the history file in the ':start:elapsed;command' format.
-setopt HIST_EXPIRE_DUPS_FIRST    # Expire a duplicate event first when trimming history.
-setopt HIST_FIND_NO_DUPS         # Do not display a previously found event.
-setopt HIST_IGNORE_ALL_DUPS      # Delete an old recorded event if a new event is a duplicate.
-setopt HIST_IGNORE_DUPS          # Do not record an event that was just recorded again.
-setopt HIST_IGNORE_SPACE         # Do not record an event starting with a space.
-setopt HIST_SAVE_NO_DUPS         # Do not write a duplicate event to the history file.
-setopt SHARE_HISTORY             # Share history between all sessions.
-
-# You Should Use Zsh plugin
-export YSU_MESSAGE_POSITION="after"
-
-# VCPkg
-if [[ -d "$HOME/.vcpkg" ]]; then
-  export VCPKG_ROOT="$HOME/.vcpkg"
-  export PATH=$VCPKG_ROOT:$PATH
-fi
-
