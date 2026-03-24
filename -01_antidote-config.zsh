feb5d5627ec70d77b895452681608f8daa219b11
Paolo Casellati <paolo.casellati@a-cube.io>
2026-03-24 18:48:53 +0100

    Added antidote config

---
+++ -01_antidote-config.zsh
@@ -0,0 +1,7 @@
+# antidote config
+#
+# look for updated settings at https://github.com/mattmc3/antidote/blob/main/templates/config.zsh
+
+zstyle ':antidote:bundle' path-style 'short'
+
+zstyle ':antidote:defer' bundle 'romkatv/zsh-defer'
