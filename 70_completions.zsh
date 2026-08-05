8f168df849fc2591feacaf8a2d3d7a6b72552178
Paolo Casellati <paolo.casellati@a-cube.io>
2026-08-05 20:28:14 +0200

    fix completion without fzf

--- 70_completions.zsh
+++ 70_completions.zsh
@@ -1,13 +1,20 @@
 zstyle ':completion:*:git-checkout:*' sort false
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-zstyle ':completion:*' menu no
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
-zstyle ':fzf-tab:complete:*:*' fzf-preview '
-  if [[ -d $realpath ]]; then eza -1 --color=always $realpath
-  elif [[ -f $realpath ]]; then bat --color=always --style=numbers --line-range=:100 $realpath
-  fi 2>/dev/null'
-zstyle ':fzf-tab:*' fzf-flags --color=fg:1,fg+:2 --bind=tab:accept
-zstyle ':fzf-tab:*' use-fzf-default-opts yes
+# fzf-tab is loaded unconditionally by antidote and steals TAB on load, so without
+# the fzf binary TAB would show nothing at all: fall back to zsh's own menu.
+if (( $+commands[fzf] )); then
+  zstyle ':completion:*' menu no
+  zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+  zstyle ':fzf-tab:complete:*:*' fzf-preview '
+    if [[ -d $realpath ]]; then eza -1 --color=always $realpath
+    elif [[ -f $realpath ]]; then bat --color=always --style=numbers --line-range=:100 $realpath
+    fi 2>/dev/null'
+  zstyle ':fzf-tab:*' fzf-flags --color=fg:1,fg+:2 --bind=tab:accept
+  zstyle ':fzf-tab:*' use-fzf-default-opts yes
+else
+  (( $+functions[disable-fzf-tab] )) && disable-fzf-tab
+  zstyle ':completion:*' menu select
+fi
 zstyle ':completion:*' matcher-list 'r:|=**'
 
 # Terraform / OpenTofu.
